771b5d48e0fed972e0bf36ddd40412cdb5379b89
lastdesire <87568302+lastdesire@users.noreply.github.com>
2021-11-27 19:40:17 +0300

    Update task-6-c.py

--- task-6-c.py
+++ task-6-c.py
@@ -4,7 +4,7 @@ for ladybug_1 in range(2, 22+1, 2):
     for ladybug_2 in range(2, 22+1, 2):
         for ladybug_3 in range(2, 22+1, 2):
             spots = ladybug_1 + ladybug_2 + ladybug_3
-            if (spots >= 54):
+            if (spots > 54):
                 good_situations += 1
             all_situations += 1
 print("good_situations = ", good_situations)
